400b0ef6ff3d0d9d10da1db4ad52b619ca5a0153
liveHarshit <live.harshit@hotmail.com>
2018-08-02 18:57:04 +0530

    print json response

--- app/src/main/java/com/liveharshit/android/dailynews/NetworkUtils.java
+++ app/src/main/java/com/liveharshit/android/dailynews/NetworkUtils.java
@@ -2,16 +2,80 @@ package com.liveharshit.android.dailynews;
 
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class NetworkUtils {
     private NetworkUtils() {
         //Required empty private constructor
+    }
+
+    public static String fetchNewsData(String requestUrl) {
+        Log.e("url", requestUrl);
+        URL url = null;
+        try {
+            url = new URL(requestUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHTTPrequest(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("Error", "Failed to make HTTP request");
+        }
+
+        return jsonResponse;
+    }
+
+    private static String makeHTTPrequest(URL url) throws IOException {
+        String jsonResponse = null;
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+
+        urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setReadTimeout(10000);
+        urlConnection.setConnectTimeout(15000);
+        urlConnection.setRequestMethod("GET");
+        urlConnection.connect();
+        if (urlConnection.getResponseCode() == 200) {
+            inputStream = urlConnection.getInputStream();
+            jsonResponse = readFromInputSteam(inputStream);
+        } else {
+            Log.e("", "Invalid Request");
+        }
+
+        Log.d("Json response", jsonResponse);
+
+        return jsonResponse;
+    }
+
+    private static String readFromInputSteam(InputStream inputStream) throws IOException {
+
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
     }
 
 
--- app/src/main/java/com/liveharshit/android/dailynews/fragments/DefaultFragment.java
+++ app/src/main/java/com/liveharshit/android/dailynews/fragments/DefaultFragment.java
@@ -1,8 +1,11 @@
 package com.liveharshit.android.dailynews.fragments;
 
 
+import com.liveharshit.android.dailynews.NetworkUtils;
+import com.liveharshit.android.dailynews.NewsItems;
 import com.liveharshit.android.dailynews.R;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,22 +14,45 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class DefaultFragment extends Fragment {
 
     public DefaultFragment() {
         // Required empty public constructor
     }
 
+    private String jsonResponse;
+    private String url;
+    private TextView alertTextView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.news_list, container, false);
+        url = getArguments().getString("API_URL");
 
-        String url = getArguments().getString("API_URL");
+        alertTextView = (TextView) rootView.findViewById(R.id.alert_text_view);
+        alertTextView.setText(jsonResponse);
 
-        TextView alertTextView = (TextView) rootView.findViewById(R.id.alert_text_view);
-        alertTextView.setText(url);
+        NewsAsyncTask task = new NewsAsyncTask();
+        task.execute(url);
 
         return rootView;
     }
+
+    private class NewsAsyncTask extends AsyncTask<String, Void, String /*ArrayList<NewsItems>*/> {
+
+        @Override
+        protected /*ArrayList<NewsItems>*/ String doInBackground(String... strings) {
+            String jsonResponse = NetworkUtils.fetchNewsData(strings[0]);
+            return jsonResponse;
+        }
+
+        @Override
+        protected void onPostExecute(String jsonResponse/*ArrayList<NewsItems> newsItems*/) {
+            alertTextView.setText(jsonResponse);
+        }
+    }
+
 }
