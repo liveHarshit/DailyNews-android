74fcdfcd21835e84cc2491e8029ad1f09e494ec1
liveHarshit <live.harshit@hotmail.com>
2018-08-02 10:40:25 +0530

    add search UI

--- app/src/main/java/com/liveharshit/android/dailynews/NewsActivity.java
+++ app/src/main/java/com/liveharshit/android/dailynews/NewsActivity.java
@@ -1,56 +1,69 @@
 package com.liveharshit.android.dailynews;
 
 import android.content.Context;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.inputmethod.EditorInfo;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.astuetz.PagerSlidingTabStrip;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 public class NewsActivity extends AppCompatActivity {
-    private EditText queryEditText;
-    private String query;
+    private MaterialSearchView searchView;
     private ViewPager viewPager;
+    private Toolbar toolbar;
+    private PagerSlidingTabStrip tabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
 
-        queryEditText = (EditText) findViewById(R.id.edit_query);
-        queryEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                    performSearch();
-                    return true;
-                }
-                return false;
-            }
-        });
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         FragmentPagerAdapter adapter = new FragmentPageAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tab);
-        tabLayout.setupWithViewPager(viewPager);
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.sliding_tabs);
+        tabs.setViewPager(viewPager);
 
+        searchView = (MaterialSearchView) findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                performSearch(query);
+                return true;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 
-    private void performSearch() {
-        query = queryEditText.getText().toString().trim();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.news_activity, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+        return true;
+    }
+
+    private void performSearch(String query) {
         Toast.makeText(this, query, Toast.LENGTH_SHORT).show();
-        queryEditText.clearFocus();
         InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        in.hideSoftInputFromWindow(queryEditText.getWindowToken(), 0);
+        in.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
+        tabs.setVisibility(View.GONE);
     }
+
 }
