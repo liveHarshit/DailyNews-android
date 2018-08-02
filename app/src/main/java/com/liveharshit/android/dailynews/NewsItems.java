309f711031c5d3a0b185484fb39c20cae0172453
liveHarshit <live.harshit@hotmail.com>
2018-08-02 18:36:32 +0530

    add newsItem function for type ArrayAdapter

--- app/src/main/java/com/liveharshit/android/dailynews/NewsItems.java
+++ app/src/main/java/com/liveharshit/android/dailynews/NewsItems.java
@@ -3,5 +3,31 @@ package com.liveharshit.android.dailynews;
 import android.widget.SearchView;
 
 public class NewsItems {
+    private String mTitle;
+    private String mDescription;
+    private String mNewsUrl;
+    private String mImageUrl;
 
+    public NewsItems(String title, String description, String newsUrl, String imageUrl) {
+        mTitle = title;
+        mDescription = description;
+        mNewsUrl = newsUrl;
+        mImageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public String getNewsUrl() {
+        return mNewsUrl;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
 }
